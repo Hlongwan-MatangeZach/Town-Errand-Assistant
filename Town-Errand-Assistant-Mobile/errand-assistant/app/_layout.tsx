0140f83c0e71d4c2bc34aa3ef9e6fcf1205680fc
Hlongwan-MatangeZach <hlongwanemz05@gmail.com>
2026-03-17 00:58:34 +0200

    Refactored the layout.tsx to have user contex

--- Town-Errand-Assistant-Mobile/errand-assistant/app/_layout.tsx
+++ Town-Errand-Assistant-Mobile/errand-assistant/app/_layout.tsx
@@ -3,15 +3,18 @@ import { Stack } from 'expo-router';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { UserProvider } from '@/context/UserContext';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-      </Stack>
-    </ThemeProvider>
+    <UserProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+        </Stack>
+      </ThemeProvider>
+    </UserProvider>
   );
 }
